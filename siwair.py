fefa0d8bfe132cb2d3349da61ea7b6c66101eba4
Nikodem Kastelik <nikodemkastelik@gmail.com>
2019-04-06 20:34:36 +0200

    2#: Modify webpage layout to contain only Order and Stations sections

--- siwair.py
+++ siwair.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask import render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('index.html', bootstrap_path="static/startbootstrap-freelancer")
+    with open("static/contents.json") as fd:
+        contents = json.load(fd)
+    return render_template('index.html',
+                           bootstrap_folder="startbootstrap-freelancer",
+                           contents=contents)
 
