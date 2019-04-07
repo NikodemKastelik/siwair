d766d520780b70c29d6d0e6bcb65258cdcedfbc9
Nikodem Kastelik <nikodemkastelik@gmail.com>
2019-04-07 21:12:25 +0200

    7#: Create simple GUI Panel for PLC master simulator

--- scripts/plc_dummy_server.py
+++ scripts/plc_dummy_server.py
@@ -1,73 +1,95 @@
 import socket
+import time
+import tkinter as tk
+import threading
+from tkinter import font
 from plc_master_manager import PlcMasterManager
-from collections import OrderedDict
+from socket_server_manager import SocketServerManager
 
-statuses = OrderedDict()
-statuses["ST10"] = {"Empty":   False,
-                    "Ongoing": False,
-                    "Error":   False}
+class DummyMasterPlc:
+    def __init__(self, server_ip, port):
+        self._sockmngr = SocketServerManager(server_ip, port)
+        self._input_thread = None
+        self._running = threading.Event()
 
-statuses["ST20"] = {"Empty":   False,
-                    "Ongoing": False,
-                    "Error":   False}
+        self._root = tk.Tk()
+        self._root.title("DummyMasterPLC")
+        self._root.protocol("WM_DELETE_WINDOW", self.stop)
+        self._root.geometry("500x500")
 
-statuses["ST30"] = {"Empty":   False,
-                    "Ongoing": False,
-                    "Error":   False}
+        self._small_font = tk.font.Font(family = 'Consolas', size = 12)
+        self._title_font = tk.font.Font(family = 'Consolas', size = 18, weight = 'bold')
 
-statuses["ST40"] = {"Empty":   False,
-                    "Ongoing": False,
-                    "Error":   False}
+        mainframe = tk.Frame(self._root, borderwidth=3, relief='ridge')
+        mainframe.pack(fill=tk.BOTH, expand=True)
 
-statuses["ST50"] = {"Empty":   False,
-                    "Ongoing": False,
-                    "Error":   False}
+        titleframe = tk.Frame(mainframe, height=100, borderwidth=3, relief='raised')
+        contentframe = tk.Frame(mainframe, borderwidth=3, relief='sunken')
+        titleframe.pack(fill=tk.X)
+        contentframe.pack(fill=tk.BOTH, expand=True)
 
-statuses["ST60"] = {"Empty":   False,
-                    "Ongoing": False,
-                    "Error":   False}
+        titlelabel = tk.Label(titleframe, text="DummyMasterPLC", font=self._title_font)
+        titlelabel.pack(fill=tk.BOTH)
 
-statuses["ST70"] = {"Empty":   False,
-                    "Ongoing": False,
-                    "Error":   False}
+        self._stations_checkboxes = {}
+        for idx, station in enumerate(PlcMasterManager.STATIONS_NAMES):
+            stationframe = tk.Frame(contentframe, borderwidth = 3, relief = 'ridge')
+            stationlabel = tk.Label(stationframe, text = "Stacja " + station, font = self._title_font)
 
-statuses["ST80"] = {"Empty":   False,
-                    "Ongoing": False,
-                    "Error":   True}
+            stationframe.grid(row = idx % 4, column = int(idx / 4), padx = 10, pady = 10, sticky = 'nsew')
+            contentframe.rowconfigure(index = idx % 4, weight = 1)
+            contentframe.columnconfigure(index = int(idx / 4), weight = 1)
+            stationlabel.pack(side = tk.TOP, fill = tk.X, pady = 10)
 
-def parseInput(data):
-    response = b''
-    if data[0] == PlcMasterManager.CMD_CODE_GET_STATUS[0]:
+            v = tk.StringVar(value = "Empty")
+            for idx, status in enumerate(PlcMasterManager.STATIONS_STATUSES_NAMES):
+                checkbox = tk.Radiobutton(stationframe, text = status, variable = v, value = status, font = self._small_font, anchor = 'w')
+                checkbox.pack(side = tk.TOP, fill = tk.X, padx = 50)
+            self._stations_checkboxes[station] = v
+
+    def _createStatusResponse(self):
         response = PlcMasterManager.CMD_CODE_GET_STATUS
-        for key in statuses:
+        for station in PlcMasterManager.STATIONS_NAMES:
             val = 0
-            if statuses[key]["Empty"]:   val |= (1 << 0)
-            if statuses[key]["Ongoing"]: val |= (1 << 1)
-            if statuses[key]["Error"]:   val |= (1 << 2)
+            setting = self._stations_checkboxes[station].get()
+            if setting == "Empty":   val |= (1 << 0)
+            if setting == "Ongoing": val |= (1 << 1)
+            if setting == "Error":   val |= (1 << 2)
             response += val.to_bytes(1, byteorder='big')
-    return response
+        return response
+
+    def _parseInput(self, data):
+        response = b''
+        if data[0] == PlcMasterManager.CMD_CODE_GET_STATUS[0]:
+            return self._createStatusResponse()
+
+    def _inputReaderLoop(self):
+        while self._running.is_set():
+            data = self._sockmngr.recv()
+            if data:
+                response = self._parseInput(data)
+                if response:
+                    self._sockmngr.send(response)
+            time.sleep(0.05)
+
+    def start(self):
+        self._sockmngr.start()
+        self._running.set()
+        self._input_thread = threading.Thread(target=self._inputReaderLoop)
+        self._input_thread.start()
+        self._root.mainloop()
+
+    def stop(self):
+        self._running.clear()
+        if self._input_thread is not None and self._input_thread.isAlive():
+            self._input_thread.join()
+        self._sockmngr.stop()
+        self._root.destroy()
+
 
 if __name__ == "__main__":
     server = "localhost"
     port = 4000
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((server, port))
-    sock.listen()
-
-    try:
-        while True:
-            print("Wait for connection")
-            conn, client_address = sock.accept()
-            print("Got connection from {}".format(client_address))
-            while True:
-                data = conn.recv(1024)
-                if data: 
-                    print("Got: {}".format(data))
-                    response = parseInput(data)
-                    if response:
-                        conn.sendall(response)
-    except KeyboardInterrupt:
-        sock.close()
-
+    dummyplc = DummyMasterPlc(server, port)
+    dummyplc.start()
