35767485cc2b5879197d444d7fb8cc6cc3402288
NikodemKastelik <nikodemkastelik@gmail.com>
2019-04-16 02:34:31 +0200

    8#: Add support for server deployment under Bash for Windows

--- scripts/env_install.sh
+++ scripts/env_install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 cd "$(dirname "$0")"
 
-python3 -m pip install virtualenv
+python3 -m pip install virtualenv --user
 python3 -m virtualenv --python=python3 venv
-source venv/bin/activate
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    source ./venv/bin/activate
+else
+    source ./venv/Scripts/activate
+fi
+
+if [ ! $? -eq 0 ];
+then
+    echo "Could not activate virtualenv."
+    exit
+fi
+
 python3 -m pip install -r requirements.txt
+deactivate
--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-source venv/bin/activate
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    source ./venv/bin/activate
+else
+    source ./venv/Scripts/activate
+fi
+
 export FLASK_APP=siwair.py
 export FLASK_ENV=development
+export PYTHONUNBUFFERED=1
 cd .. && flask run --host=0.0.0.0
