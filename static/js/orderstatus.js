e7f61520e1473bcc8f640af81f30ba4922c32ab5
Pawel <theqemist.pawel@gmail.com>
2019-05-27 22:42:47 +0200

    22#: Improve frontend graphics
    Added product bracket animations. Changed station icons to the custom images.
    Added the cart total indicator and the queue total indicator.
    Improved visual experience and fixed minor bugs related to webpage apperance.

--- static/js/orderstatus.js
+++ static/js/orderstatus.js
@@ -1,5 +1,17 @@
 // ========== reusable functions ==========
 
+function updateOrderTotal() {
+    let numberOfParts = 0;
+    document.getElementById("order-status").querySelectorAll(".order-status-item").forEach( function (e) {
+        let number = parseInt(e.getAttribute("data-quantity"));
+        if (isNaN(number)) {
+            number = 0;
+        }
+        numberOfParts += number;
+    });
+    document.getElementById("order-status-total").innerHTML = numberOfParts.toString();
+}
+
 // send order removal request
 function orderDeleteItem(item) {
   var orderItem = item.closest(".order-status-item");
@@ -10,7 +22,6 @@ function orderDeleteItem(item) {
                 dataType:"json",
                 data: JSON.stringify(orderItem.id)
   });
-  console.log("delete:" + orderItem.id);
 }
 
 // update current order
@@ -19,7 +30,7 @@ function orderCurrentStatusUpdate(){
     if ("id" in data){
 
       // get current order status element
-      var currentItem = document.getElementById("order-status-current-item-id");
+      var currentItem = document.getElementById("order-status-current-item");
       var addCurrentItem = false;
 
       // check if the current ordet status elelment is null (no such element)
@@ -29,11 +40,12 @@ function orderCurrentStatusUpdate(){
       }
       else{
         // check if current element is received element
-        if (currentItem.closest(".order-status-current-item").id.toString().toUpperCase() != data.id.toString().toUpperCase()){
+        if (currentItem == null) {
+            addCurrentItem = true;
+        } else if (currentItem.getAttribute("data-id").toString().toUpperCase() != data.id.toString().toUpperCase()){
           // id is different
-          addCurrentItem = true
-        }
-        else{
+          addCurrentItem = true;
+        } else {
           // check sum of part quantity
           quantitySum = 0;
           data.contents.forEach(function(element) {
@@ -56,6 +68,25 @@ function orderCurrentStatusUpdate(){
 
         document.querySelectorAll(".order-status-current-item").forEach(function(element) {
           parent.removeChild(element);
+            if (!(element.classList.contains("order-status-current-empty"))) {
+                element.classList.remove("order-status-current-item");
+                element.classList.add("order-status-done-item");
+                element.id = "";
+                let date = new Date();
+                element.endDate = "dnia: " + (parseInt(date.getMonth()) + 1).toString() + "." + date.getDate() + " o godzinie: " + date.getHours() + ":" + ("0" + date.getMinutes()).slice(-2);
+                element.children[1].innerHTML = `
+                    <div class="d-flex flex-column col-12 justify-content-around align-items-center align-content-center text-center">
+                        <div class="item-text">
+                          <p id="order-status-done-time" class="font-weight-bold mb-0"> Zakończono: ${ element.endDate.toString() }</p>
+                        </div>
+                    </div>
+                `;
+                document.getElementById("order-status-done").appendChild(element);
+                setTimeout( function () {
+                    var orderDoneStatus = document.getElementById("order-status-done");
+                    orderDoneStatus.removeChild(element);
+                }, 10000);
+            }
         });
 
         // HTML element to be appended
@@ -65,7 +96,6 @@ function orderCurrentStatusUpdate(){
           "d-flex",
           "justify-content-between",
           "align-items-center",
-          "text-uppercase",
           "my-3",
           "row"
         );
@@ -73,33 +103,38 @@ function orderCurrentStatusUpdate(){
         data.contents.forEach(function(element) {
           quantitySum += parseInt(element.quantity);
         });
+        currentOrderItem.id = "order-status-current-item";
         currentOrderItem.setAttribute("data-quantity",quantitySum.toString());
-        currentOrderItem.id = data.id.toString();
-        currentOrderItem.products = "";
+        currentOrderItem.setAttribute("data-id", data.id.toString());
+        currentOrderItem.products = "<ul class=\"order-part-list\">";
         data.contents.forEach(function(element) {
-          currentOrderItem.products += element.quantity.toString() + "x " + element.recipe.toString() + ", ";
+            currentOrderItem.products += "<li>" +  element.quantity.toString() + "x  " + element.recipe.toString() + "</li>";
         });
-        currentOrderItem.products = currentOrderItem.products.substring(0, currentOrderItem.products.length-2);
+        currentOrderItem.products += "</ul>";
 
         // element HTML
         currentOrderItem.innerHTML = `
-        <!-- cart item -->
-          <div class="d-flex flex-column col-xl-6 col-md-12 justify-content-around align-items-center align-content-center">
-            <div class="item-text">
-              <p id="order-status-current-item-id" class="font-weight-bold mb-0"> Nr zamówienia: ${ currentOrderItem.id }</p>
-            </div>
-            <div class="item-text">
-              <p id="order-status-current-item-products" class="font-weight-bold mb-0">Zawartość zamówienia: ${ currentOrderItem.products }</p>
-            </div>
-          </div>
-          <div class="col-xl-6 col-md-12">
-            <div class="progress" style="height:25px">
-              <div class="progress-bar progress-bar-striped progress-bar-animated" id="progressbar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%; height: 25px">
-              </div>
-            </div>
-          </div>
-        <!--end of  cart item -->
-        `;
+            <!-- cart item -->
+                <div class="d-flex flex-wrap flex-row col-12 justify-content-around align-items-center align-content-center">
+                    <div class="d-flex col-sm-12 col-md-6 justify-content-around align-items-center align-content-center">
+                        <div class="item-text">
+                            <p id="order-status-item-id" class="font-weight-bold mb-0"> Nr zamówienia: ${ currentOrderItem.getAttribute("data-id").toString() }</p>
+                        </div>
+                    </div>
+                    <div class="d-flex col-sm-12 col-md-6 justify-content-around align-items-center align-content-center">
+                        <div class="item-text">
+                            <p id="order-status--item-products" class="font-weight-bold mb-0">Zawartość zamówienia: ${ currentOrderItem.products }</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="progress" style="height:25px">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" id="progressbar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%; height: 25px">
+                        </div>
+                    </div>
+                </div>
+          <!--end of  cart item -->
+          `;
 
         document.getElementById("order-status-current").appendChild(currentOrderItem);
       }
@@ -112,29 +147,55 @@ function orderCurrentStatusUpdate(){
     }
     else{
 
+      let appendEmpty = true;
       // delete current order element
       document.querySelectorAll(".order-status-current-item").forEach(function(element) {
         var orderCurrentStatus = element.closest(".order-status-current");
-        orderCurrentStatus.removeChild(element);
+        if (element.classList.contains("order-status-current-empty")) {
+            appendEmpty = false;
+        } else {
+            orderCurrentStatus.removeChild(element);
+        }
+        if (!(element.classList.contains("order-status-current-empty"))) {
+            element.classList.remove("order-status-current-item");
+            element.classList.add("order-status-done-item");
+            element.id = "";
+            let date = new Date();
+            element.endDate = "dnia: " + (parseInt(date.getMonth()) + 1).toString() + "." + date.getDate() + " o godzinie: " + date.getHours() + ":" + ("0" + date.getMinutes()).slice(-2);
+            element.children[1].innerHTML = `
+                <div class="d-flex flex-column col-12 justify-content-around align-items-center align-content-center text-center">
+                    <div class="item-text">
+                      <p id="order-status-done-time" class="font-weight-bold mb-0"> Zakończono: ${ element.endDate.toString() }</p>
+                    </div>
+                </div>
+            `;
+            document.getElementById("order-status-done").appendChild(element);
+            setTimeout( function () {
+                var orderDoneStatus = document.getElementById("order-status-done");
+                orderDoneStatus.removeChild(element);
+            }, 10000);
+        }
       });
 
-      // insert empty element
-      const orderCurrentStatusEmptyItem = document.createElement("div");
-      orderCurrentStatusEmptyItem.classList.add(
-        "order-status-current-item",
-        "d-flex",
-        "align-items-center",
-        "my-3",
-        "order-status-current-empty"
-      );
+      if (appendEmpty) {
+          // insert empty element
+          const orderCurrentStatusEmptyItem = document.createElement("div");
+          orderCurrentStatusEmptyItem.classList.add(
+            "order-status-current-item",
+            "d-flex",
+            "align-items-center",
+            "my-3",
+            "order-status-current-empty"
+          );
 
-      orderCurrentStatusEmptyItem.innerHTML =`
-      <!-- empty cart item -->
-        <h4 class="text-center text-uppercase text-white center"> Brak zamówień w produkcji </h4>
-      <!--end of empty cart item -->
-      `;
-       var orderStatus = document.getElementById("order-status-current");
-      orderStatus.appendChild(orderCurrentStatusEmptyItem);
+          orderCurrentStatusEmptyItem.innerHTML =`
+          <!-- empty cart item -->
+            <h4 class="text-center text-uppercase text-white center"> Brak zamówień w produkcji </h4>
+          <!--end of empty cart item -->
+          `;
+           var orderStatus = document.getElementById("order-status-current");
+          orderStatus.appendChild(orderCurrentStatusEmptyItem);
+      }
     }
   })
 }
@@ -148,8 +209,15 @@ function orderStatusUpdate(){
     Array.from(document.getElementById("order-status").children).forEach(function(element) {
       if (! element.classList.contains("order-status-empty")){
         if (data.find(o => o.id === element.id.toString()) == null){
-          let parent = element.closest(".order-status");
-          parent.removeChild(element);
+          element.addEventListener("animationend", function () {
+              let container = this.closest(".order-status");
+              if (!(container == null)) {
+                  container.removeChild(this);
+                  updateOrderTotal();
+              }
+          });
+          element.classList.add("delete-status-item");
+    
         }
       }
     });
@@ -201,7 +269,6 @@ function orderStatusUpdate(){
             "d-flex",
             "justify-content-between",
             "align-items-center",
-            "text-capitalize",
             "my-3",
             "row"
           );
@@ -211,28 +278,35 @@ function orderStatusUpdate(){
           });
           currentOrderItem.setAttribute("data-quantity",quantitySum.toString());
           currentOrderItem.id = element.id.toString();
-          currentOrderItem.products = "";
+          currentOrderItem.products = "<ul class=\"order-part-list\">";
+;
           element.contents.forEach(function(element) {
-            currentOrderItem.products += element.quantity.toString() + "x " + element.recipe.toString() + ", ";
+            currentOrderItem.products += "<li>" +  element.quantity.toString() + "x  " + element.recipe.toString() + "</li>";
           });
-          currentOrderItem.products = currentOrderItem.products.substring(0, currentOrderItem.products.length-2);
+          currentOrderItem.products += "</ul>";
 
           // element HTML
-          currentOrderItem.innerHTML = `
-          <!-- cart item -->
-            <div class="d-flex justify-content-around align-items-center align-content-center">
-              <div class="item-text">
-                <p id="order-status-item-id" class="font-weight-bold mb-0">${ currentOrderItem.id }</p>
-              </div>
-              <div class="item-text">
-                <p id="order-status-item-products" class="font-weight-bold mb-0">${ currentOrderItem.products }</p>
-              </div>
-              <button id="order-status-item-remove" class="oder-status-delete-item-button btn order-status-item-remove">
-                <i class="fas fa-trash fa-1x"></i>
-              </button>
-            </div>
-          <!--end of  cart item -->
-          `;
+        currentOrderItem.innerHTML = `
+            <!-- cart item -->
+                <div class="d-flex flex-wrap flex-row col-10 justify-content-around align-items-center align-content-center">
+                    <div class="d-flex col-sm-10 col-md-5 justify-content-around align-items-center align-content-center">
+                        <div class="item-text">
+                            <p id="order-status-item-id" class="font-weight-bold mb-0"> Nr zamówienia: ${ currentOrderItem.id }</p>
+                        </div>
+                    </div>
+                    <div class="d-flex col-sm-10 col-md-5 justify-content-around align-items-center align-content-center">
+                        <div class="item-text">
+                            <p id="order-status--item-products" class="font-weight-bold mb-0">Zawartość zamówienia: ${ currentOrderItem.products }</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex flex-column col-2 justify-content-around align-items-center align-content-center">
+                    <button id="order-status-item-remove" class="order-status-delete-item-button btn order-status-item-remove">
+                        <i class="fas fa-trash fa-1x"></i>
+                    </button>
+                </div>
+            <!--end of  cart item -->
+        `;
 
           document.getElementById("order-status").appendChild(currentOrderItem);
 
@@ -242,38 +316,48 @@ function orderStatusUpdate(){
             let del = deleteEvent.target;
             orderDeleteItem(del);
           });
+
+        updateOrderTotal();
+
         }
         else{
           // do not add an element
         }
       })
-    }
-    else{
+    } else{
 
 
       var orderStatusList = document.getElementById("order-status");
       // delete current order element
+      let appendEmpty = true;
       document.querySelectorAll(".order-status-item").forEach(function(element) {
-        orderStatusList.removeChild(element);
+        if (element.classList.contains("order-status-empty")) {
+            appendEmpty = false;
+        } else {
+            orderStatusList.removeChild(element);
+        }
       });
 
-      // insert empty element
-      const orderStatusEmptyItem = document.createElement("div");
-      orderStatusEmptyItem.classList.add(
-        "order-status-item",
-        "d-flex",
-        "align-items-center",
-        "my-3",
-        "order-status-empty"
-      );
+      if (appendEmpty) {
+          // insert empty element
+          const orderStatusEmptyItem = document.createElement("div");
+          orderStatusEmptyItem.classList.add(
+            "order-status-item",
+            "d-flex",
+            "align-items-center",
+            "my-3",
+            "order-status-empty"
+          );
 
-      orderStatusEmptyItem.innerHTML =`
-      <!-- empty cart item -->
-        <h4 class="text-center text-uppercase text-white center"> Brak zamówień w kolejce </h4>
-      <!--end of empty cart item -->
-      `;
+          orderStatusEmptyItem.innerHTML =`
+          <!-- empty cart item -->
+            <h4 class="text-center text-uppercase text-white center"> Brak zamówień w kolejce </h4>
+          <!--end of empty cart item -->
+          `;
 
-      orderStatusList.appendChild(orderStatusEmptyItem);
+          orderStatusList.appendChild(orderStatusEmptyItem);
+      }
+      updateOrderTotal();
     }
   })
 }
